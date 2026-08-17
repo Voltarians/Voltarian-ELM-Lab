80aca608ad9f5664d5472b98260948b478843afc
Monroe Lee King Jr <monroelkjr@gmail.com>
2026-08-17 14:16:16 -0500

    Add app/build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    id("com.android.application")
+    kotlin("android")
+}
+
+android {
+    namespace = "org.voltarians.elmlab.android"
+    compileSdk = 35
+    defaultConfig {
+        applicationId = "org.voltarians.elmlab"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 1
+        versionName = "0.1.0"
+    }
+}
+
+dependencies {
+    implementation(project(":elm-core"))
+    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+}
+
