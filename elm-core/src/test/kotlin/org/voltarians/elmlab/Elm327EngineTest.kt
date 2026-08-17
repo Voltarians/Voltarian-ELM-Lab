58b1410c973eccbf19c09aad78c66cd1b973e254
Monroe Lee King Jr <monroelkjr@gmail.com>
2026-08-17 14:23:13 -0500

    Add discovery and controls: elm-core/src/test/kotlin/org/voltarians/elmlab/Elm327EngineTest.kt

--- elm-core/src/test/kotlin/org/voltarians/elmlab/Elm327EngineTest.kt
+++ elm-core/src/test/kotlin/org/voltarians/elmlab/Elm327EngineTest.kt
@@ -7,5 +7,10 @@ class Elm327EngineTest {
     @Test fun identityAndPrompt() = assertTrue(Elm327Engine().execute("ATI").endsWith("ELM327 v1.5\r>"))
     @Test fun voltage() = assertTrue(Elm327Engine(VehicleProfile(supplyVoltage = 12.1)).execute("ATRV").contains("12.1V"))
     @Test fun speedPid() = assertTrue(Elm327Engine(VehicleProfile(speedKph = 42)).execute("010D").contains("41 0D 2A"))
+    @Test fun remoteProfileControl() {
+        val engine = Elm327Engine()
+        assertTrue(engine.execute("ATVSETSPD42").contains("OK"))
+        assertTrue(engine.execute("010D").contains("41 0D 2A"))
+        assertTrue(engine.execute("ATVPROFILE").contains("SPD=42"))
+    }
 }
-
